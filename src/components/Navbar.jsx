95e7161635e4b7707c7ad52ef32555b48388613f
Isaac Berrio <berriisaax18@hotmail.com>
2024-02-29 13:08:40 -0500

    Mejorando la navegacion entre secciones con el navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,42 @@
 import React from "react";
 
 const Navbar = () => {
+  const handleClick = (type) => {
+    if (type === "Home") {
+      window.scrollTo({
+        top: 0
+      });
+    } else if (type === "About") {
+      window.scrollTo({
+        top: 700
+      })
+    } else if (type === "Project") {
+      window.scrollTo({
+        top: 1400
+      })
+    } else if (type === "Skills") {
+      window.scrollTo({
+        top: 2500
+      })
+    }
+  };
+
   return (
     <nav className="navbar animate__animated animate__zoomInUp">
       <h3>I:Dev</h3>
       <ul className="links">
-        <a href="#Home">
-          <li>Home</li>
-        </a>
-        <a href="#About">
-          <li>About</li>
-        </a>
-        <a href="#Project">
-          <li>Projects</li>
-        </a>
-        <a href="#Skills">
-          <li>Skills</li>
-        </a>
+        <li onClick={() =>handleClick("Home")} >
+          <a href="#Home">Home</a>
+        </li>
+        <li onClick={() =>handleClick("About")}>
+          <a href="#About">About</a>
+        </li>
+        <li onClick={() =>handleClick("Project")}>
+          <a href="#Project">Projects</a>
+        </li>
+        <li onClick={() =>handleClick("Skills")}>
+          <a href="#Skills">Skills</a>
+        </li>
       </ul>
     </nav>
   );
